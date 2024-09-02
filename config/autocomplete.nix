20a9f949985f85cb3c0dab6ce633a45b55feb7d9
Frederik Richter <frederik.richter@mailbox.org>
2024-09-02 19:44:03 +0200

    added cmp config

---
+++ config/autocomplete.nix
@@ -0,0 +1,22 @@
+{
+    plugins.cmp = {
+        autoEnableSources = true;
+        settings.sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+        { name = "luasnip"; }
+        { name = "cmdline"; }
+        { name = "git"; }
+        { name = "greek"; }
+        { name = "nvim_lua"; }
+        { name = "pandoc_nvim"; }
+        { name = "treesitter"; }
+        { name = "rg"; }
+        { name = "tmux"; }
+        { name = "zsh"; }
+        { name = "luasnip"; }
+        { name = "latex_symbols"; }
+        ];
+    };
+}
