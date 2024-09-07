8e32a3ced2c6ce19bccc2f3de9b2fa9ee1bf4a75
Frederik Richter <frederik.richter@mailbox.org>
2024-09-07 16:49:44 +0200

    nice autocomplete keybinds in cmp

--- config/autocomplete.nix
+++ config/autocomplete.nix
@@ -2,22 +2,45 @@
     plugins.cmp = {
         enable = true;
         autoEnableSources = true;
-        settings.sources = [
-        { name = "nvim_lsp"; }
-        { name = "path"; }
-        { name = "buffer"; }
-        { name = "luasnip"; }
-        { name = "cmdline"; }
-        { name = "git"; }
-        { name = "greek"; }
-        { name = "nvim_lua"; }
-        { name = "pandoc_nvim"; }
-        { name = "treesitter"; }
-        { name = "rg"; }
-        { name = "tmux"; }
-        { name = "zsh"; }
-        { name = "luasnip"; }
-        { name = "latex_symbols"; }
-        ];
+        settings = {
+            mapping = {
+                __raw = ''
+                    cmp.mapping.preset.insert({
+                            ['<Tab>'] = cmp.mapping(function(fallback)
+                                    if cmp.visible() then
+                                    cmp.select_next_item()
+                                    else
+                                    cmp.complete()
+                                    end
+                                    end, { 'i', 's' }),
+                            ['<S-Tab>'] = cmp.mapping(function(fallback)
+                                    if cmp.visible() then
+                                    cmp.select_prev_item()
+                                    else
+                                    fallback()
+                                    end
+                                    end, { 'i', 's' }),
+                            ['<C-e>'] = cmp.mapping.abort()
+                            })
+                '';
+                };
+            sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+            { name = "luasnip"; }
+            { name = "cmdline"; }
+            { name = "git"; }
+            { name = "greek"; }
+            { name = "nvim_lua"; }
+            { name = "pandoc_nvim"; }
+            { name = "treesitter"; }
+            { name = "rg"; }
+            { name = "tmux"; }
+            { name = "zsh"; }
+            { name = "luasnip"; }
+            { name = "latex_symbols"; }
+            ];
+        };
     };
 }
