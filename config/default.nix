4d82f9a0abd66bae986b6e6339b31442726817be
Frederik Richter <frederik.richter@mailbox.org>
2024-09-08 14:18:04 +0200

    enabled oil

--- config/default.nix
+++ config/default.nix
@@ -3,11 +3,13 @@
   imports = [ 
     ./nixvim.nix
     ./autocomplete.nix
+    ./autosession.nix
     ./betterescape.nix
     ./chadtree.nix
     ./dashboard.nix
     ./fugitive.nix
     ./telescope.nix
     ./vimtex.nix
+    ./oil.nix
   ];
 }
