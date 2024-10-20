6037d4fdc504762a18eb51aca1591f863001720e
Frederik Richter <frederik.richter@mailbox.org>
2024-10-20 17:51:52 +0200

    added output dir

--- config/typst.nix
+++ config/typst.nix
@@ -17,7 +17,7 @@ local function execute_on_save(filename)
     print(result)
 
 
-    local compile = "typst compile " .. filename .. " --root='../'"
+    local compile = "typst compile " .. filename .. " --root='../' pdf/output.pdf"
     result = vim.fn.system(compile)  -- Attempt to execute the command
     print(result)
 end
