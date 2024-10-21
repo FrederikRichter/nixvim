e0aa5a44659461403d0cf045aa461f58ccbaf5d7
Frederik Richter <frederik.richter@mailbox.org>
2024-10-21 17:46:35 +0200

    add tinymist dep

--- config/typst.nix
+++ config/typst.nix
@@ -2,37 +2,35 @@
 {
 extraPackages = with pkgs; [
     typst
-    typstyle
-    # lua51Packages.lyaml
-    # luajitPackages.lyaml
+    tinymist
 ];
 
 # AUTOFMT
-extraConfigLuaPost = ''
--- Define a function to execute your command with the filename
-local function execute_on_save(filename)
-    local autofmt = "typstyle -i " .. filename
-    result = vim.fn.system(autofmt)  -- Attempt to execute the command
-    vim.cmd("edit!")
-    print(result)
-
-
-    local compile = "typst compile " .. filename .. " --root='/home/frederik/git/typkasten/' /home/frederik/git/typkasten/data/pdf/output.pdf"
-    result = vim.fn.system(compile)  -- Attempt to execute the command
-    print(result)
-end
-
--- Create an autocommand group to avoid duplicate commands
-vim.api.nvim_create_augroup("TypFileCommands", { clear = true })
-
--- Set up an autocommand that triggers on saving .typ files
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = "*/data/*.typ",
-    callback = function()
-        -- Get the current filename
-        local filename = vim.fn.expand("%:p")  -- Get the full path of the file
-        execute_on_save(filename)  -- Call the function with the filename
-    end,
-    group = "TypFileCommands",
-})'';
+# extraConfigLuaPost = ''
+# -- Define a function to execute your command with the filename
+# local function execute_on_save(filename)
+#     local autofmt = "typstyle -i " .. filename
+#     result = vim.fn.system(autofmt)  -- Attempt to execute the command
+#     vim.cmd("edit!")
+#     print(result)
+#
+#
+#     local compile = "typst compile " .. filename .. " --root='/home/frederik/git/typkasten/' /home/frederik/git/typkasten/data/pdf/output.pdf"
+#     result = vim.fn.system(compile)  -- Attempt to execute the command
+#     print(result)
+# end
+#
+# -- Create an autocommand group to avoid duplicate commands
+# vim.api.nvim_create_augroup("TypFileCommands", { clear = true })
+#
+# -- Set up an autocommand that triggers on saving .typ files
+# vim.api.nvim_create_autocmd("BufWritePost", {
+#     pattern = "*/data/*.typ",
+#     callback = function()
+#         -- Get the current filename
+#         local filename = vim.fn.expand("%:p")  -- Get the full path of the file
+#         execute_on_save(filename)  -- Call the function with the filename
+#     end,
+#     group = "TypFileCommands",
+# })'';
 }
