c3c15aa0a6db8a3d121c31f034f831e265155857
Frederik Richter <frederik.richter@mailbox.org>
2025-05-31 23:51:52 +0200

    lazygit: adds keymaps

--- config/lazygit.nix
+++ config/lazygit.nix
@@ -3,4 +3,16 @@
 plugins.lazygit = {
 enable = true;
 };
+
+keymaps = [
+      {
+    action = "<cmd>LazyGit<CR>";
+    key = "<leader>g";
+    options = {
+      silent = true;
+    };
+  }
+];
+
+
 }
