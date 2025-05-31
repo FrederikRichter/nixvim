c0c4344f7005ab8cbaddfdb9a68ae135745a846c
Frederik Richter <frederik.richter@mailbox.org>
2025-06-01 00:00:25 +0200

    typst: toggle preview keymap

--- config/typst.nix
+++ config/typst.nix
@@ -4,4 +4,15 @@ extraPackages = with pkgs; [
     typst
 ];
 plugins.typst-preview.enable = true;
+
+keymaps = [
+      {
+    action = "<cmd>TypstPreviewToggle<CR>";
+    key = "<leader>t";
+    options = {
+      silent = true;
+    };
+  }
+];
+
  }
